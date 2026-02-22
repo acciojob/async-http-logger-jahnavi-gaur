7dc6b6770481a0c2ffee2faa0b985c5d6693ecc3
jahnavi-gaur <121301783+jahnavi-gaur@users.noreply.github.com>
2026-02-22 10:36:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 async function fetchData(){
 	const res = fetch('https://jsonplaceholder.typicode.com/todos/1')
-	const data = await res.json()
-	console.log(data.title)
+	// const data = await res.json()
+	console.log(res)
 }
